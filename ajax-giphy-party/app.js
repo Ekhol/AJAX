1a60a1dc4bebc166ad71ae050f02f1e41a314fb3
Nathan <frost.nathan1995@gmail.com>
2022-05-05 17:39:01 -0400

    finished JS and HTML

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,27 @@
-console.log("Let's get this party started!");
+const $searchInput = $("#searchBar");
+const $gifs = $("#gifs");
+
+function gifAdd(response) {
+    let resultLength = response.data.length;
+    if (resultLength) {
+        let randIdx = Math.floor(Math.random() * resultLength);
+        let $newColumn = $("<div>", { class: "column" });
+        let $newGif = $("<img>", { src: response.data[randIdx].images.original.url, class: "gif" });
+
+        $newColumn.append($newGif);
+        $gifs.append($newColumn);
+    };
+};
+
+$("form").on("submit", async function (e) {
+    e.preventDefault();
+    let gifSearch = $searchInput.val();
+    $searchInput.val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: gifSearch, api_key: "LMJSvS2XQNHMJI7lsMKARUiAxI8T4y5g" } });
+    gifAdd(response.data);
+});
+
+$("#resetButton").on("click", function () {
+    $gifs.empty();
+});
